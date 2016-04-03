8dec2e7add4af8a23bc566a919b51edb675daf69
nexotekHD <florianwagner.bremen@googlemail.com>
2016-04-03 21:41:16 +0200

    setup unitcreator

---
+++ UnitCreator/src/main/java/de/bergwerklabs/tryjump/unitcreator/UnitCreator.java
@@ -0,0 +1,20 @@
+package de.bergwerklabs.tryjump.unitcreator;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+/**
+ * Created by nexotekHD on 03.04.2016.
+ */
+public class UnitCreator extends JavaPlugin {
+
+    public void onEnable()
+    {
+
+    }
+
+
+    public void onDisable()
+    {
+
+    }
+}
