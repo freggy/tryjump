234f30ecbc6d5bc2dccd2902d0a04901b6427ff4
freggy <riegeryannic@gmail.com>
2017-12-16 00:42:57 +0100

    Topfed um to Atlantis

---
+++ Gameserver/src/main/java/de/bergwerklabs/tryjump/gameserver/util/AtlantisStatsWrapper.java
@@ -0,0 +1,10 @@
+package de.bergwerklabs.tryjump.gameserver.util;
+
+/**
+ * Created by Yannic Rieger on 14.12.2017.
+ * <p>
+ *
+ * @author Yannic Rieger
+ */
+public class AtlantisStatsWrapper {
+}
