e01305ac3c901144c08d74531b2a3c18f915a869
freggy <riegeryannic@gmail.com>
2018-02-11 17:08:21 +0100

    Add listener files

---
+++ tryjump-core/src/main/java/de/bergwerklabs/tryjump/gameserver/listener/EntityInteractEntityListener.java
@@ -0,0 +1,11 @@
+package de.bergwerklabs.tryjump.gameserver.listener;
+
+/**
+ * Created by Yannic Rieger on 11.02.2018.
+ * <p>
+ *
+ * @author Yannic Rieger
+ */
+public class EntityInteractEntityListener {
+
+}
